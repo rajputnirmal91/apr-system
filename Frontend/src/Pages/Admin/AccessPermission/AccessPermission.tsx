3f6e50930b761634fb13fc1b5c021cfc62bce963
rajputnirmal91 <rajputnirmal91@gmail.com>
2026-05-06 16:23:06 +0530

    Update AccessPermission.tsx

--- Frontend/src/Pages/Admin/AccessPermission/AccessPermission.tsx
+++ Frontend/src/Pages/Admin/AccessPermission/AccessPermission.tsx
@@ -127,7 +127,7 @@ function AccessPermission() {
     return () => {
       document.removeEventListener('mousedown', handleOutsideClick)
     }
-  }, [selectedEmployee,showUnsavedModal,showUnsavedModal,hasUnsavedChanges,hasUnsavedChanges])
+  }, [selectedEmployee,showUnsavedModal,showUnsavedModal,hasUnsavedChanges])
 
   const handleEmployeeSelect = (employeeId: string) => {
     if (hasUnsavedChanges && employeeId !== selectedEmployee) {
